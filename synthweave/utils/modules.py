70c2c12f3425137e7f3f6c2b36019891c311c2a2
Woleek <d.woleek@gmail.com>
2025-06-17 15:30:20 +0200

    Updated pipeline inference

--- synthweave/utils/modules.py
+++ synthweave/utils/modules.py
@@ -2,6 +2,16 @@ from torch import nn
 import torch.nn.functional as F
 from collections import OrderedDict
 
+class LazyLinearXavier(nn.LazyLinear):
+    def __init__(self, out_features: int):
+        super(LazyLinearXavier, self).__init__(out_features)
+
+    def reset_parameters(self):
+        if not self.has_uninitialized_params() and self.in_features != 0:
+            nn.init.xavier_uniform_(self.weight)
+            if self.bias is not None:
+                nn.init.constant_(self.bias, 0.01)
+
 class LinearXavier(nn.Linear):
     """A linear layer with Xavier uniform initialization."""
 
